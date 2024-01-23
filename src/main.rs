9278fcb6ca5d504ac3f0f8ab05025220d84bba6d
trey-rosius <treyrosius@gmail.com>
2024-01-23 09:53:47 +0100

    more updates

--- src/main.rs
+++ src/main.rs
@@ -8,30 +8,7 @@ fn main() {
         user_id: String::from("1"),
         email: String::from("1"),
     };
-    println!("{:?}", stream);
-    let stream2 = Stream {
-        user_id: String::from("1"),
-        email: String::from("1"),
-    };
-    println!("{:?}", stream2);
-    let stream3 = Stream {
-        user_id: String::from("1"),
-        email: String::from("1"),
-    };
-    println!("{:?}", stream3);
-    let stream4 = Stream {
-        user_id: String::from("1"),
-        email: String::from("1"),
-    };
-    println!("{:?}", stream4);
-    let stream5 = Stream {
-        user_id: String::from("1"),
-        email: String::from("1"),
-    };
-    println!("{:?}", stream5);
-    let stream6 = Stream {
-        user_id: String::from("1"),
-        email: String::from("1"),
-    };
-    println!("Hello, world!");
+    println!(" user id is {} and email is {}", stream.user_id, stream.email); 
+
+
 }
