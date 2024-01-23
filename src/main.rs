e0018ae289b425f2a24379485e23b965c39ab5d3
trey-rosius <treyrosius@gmail.com>
2024-01-23 09:46:27 +0100

    added ci pipeline

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+#[derive(Debug)]
+struct Stream {
+    user_id: String,
+    email: String,
+}
 fn main() {
+    let stream = Stream {
+        user_id: String::from("1"),
+        email: String::from("1"),
+    };
+    println!("{:?}", stream);
+    let stream2 = Stream {
+        user_id: String::from("1"),
+        email: String::from("1"),
+    };
+    println!("{:?}", stream2);
+    let stream3 = Stream {
+        user_id: String::from("1"),
+        email: String::from("1"),
+    };
+    println!("{:?}", stream3);
+    let stream4 = Stream {
+        user_id: String::from("1"),
+        email: String::from("1"),
+    };
+    println!("{:?}", stream4);
+    let stream5 = Stream {
+        user_id: String::from("1"),
+        email: String::from("1"),
+    };
+    println!("{:?}", stream5);
+    let stream6 = Stream {
+        user_id: String::from("1"),
+        email: String::from("1"),
+    };
     println!("Hello, world!");
 }
