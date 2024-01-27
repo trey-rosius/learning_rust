bf95e1e68aedd6f6bcca08039d58696e24e7ecb7
trey-rosius <treyrosius@gmail.com>
2024-01-27 23:18:52 +0100

    instrumented futures

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -3,6 +3,7 @@ use chrono::Utc;
 use serde::Deserialize;
 use sqlx::PgPool;
 use uuid::Uuid;
+use tracing::Instrument;
 #[derive(Deserialize)]
 struct FormData {
     email: String,
@@ -17,13 +18,15 @@ async fn subscribe(
     // We are using the same interpolation syntax of `println`/`print` here! log::info!(
 
         let request_id = Uuid::new_v4();
-    tracing::info!(
-        "request_id {} - Adding '{}' '{}' as a new subscriber.",
-        request_id,
-        form.email,
-        form.name,
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber",
+        %request_id,
+        subscriber_email = %form.email,
+        subscriber_name = %form.name,
     );
-    tracing::info!("request_id: {} - Saving new subscriber details in the database ",request_id);
+
+    let _request_span_guard = request_span.enter();
+    let query_span = tracing::info_span!("Saving new subscriber details in the database ");
 
     match sqlx::query!(
         r#"
@@ -34,7 +37,8 @@ async fn subscribe(
         form.name,
         Utc::now()
     )
-    .execute(pool.get_ref())
+    .execute(pool.as_ref())
+    .instrument(query_span)
     .await
     {
         Ok(_) => {
